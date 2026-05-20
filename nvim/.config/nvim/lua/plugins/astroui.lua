7ed8a60a7f8824effc4b50d83fe73f608e12efd3
Vinicius Gomes <vinicius.svgomes@gmail.com>
2026-05-20 20:47:11 -0300

    Migrate AstroNvim to v6 (and Neovim to 0.12 as well)

--- nvim/.config/nvim/lua/plugins/astroui.lua
+++ nvim/.config/nvim/lua/plugins/astroui.lua
@@ -1,3 +1,5 @@
+-- if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
+
 -- AstroUI provides the basis for configuring the AstroNvim User Interface
 -- Configuration documentation can be found with `:h astroui`
 -- NOTE: We highly recommend setting up the Lua Language Server (`:LspInstall lua_ls`)
@@ -9,7 +11,6 @@ return {
   ---@type AstroUIOpts
   opts = {
     -- change colorscheme
-    -- colorscheme = "catppuccin-mocha",
     colorscheme = "tokyonight-night",
     -- AstroUI allows you to easily modify highlight groups easily for any and all colorschemes
     highlights = {
--- nvim/.config/nvim/lua/plugins/theme.lua
+++ nvim/.config/nvim/lua/plugins/theme.lua
@@ -2,13 +2,16 @@ return {
   {
     "catppuccin/nvim",
     name = "catppuccin",
-    opts = {
-      -- configuration options...
-    },
+    opts = {},
   },
   {
     "folke/tokyonight.nvim",
     lazy = false,
+    opts = {},
+  },
+  {
+    "rose-pine/neovim",
+    name = "rose-pine",
     opts = {},
   },
 }
